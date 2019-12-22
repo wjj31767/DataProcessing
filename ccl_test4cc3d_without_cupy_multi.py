a38d71869c74aa9dbdd32c6806d3fd5b749af313
jinwei <wsy317672256@gmail.com>
2019-12-22 16:19:27 +0100

    multi finished but seems crashed when 5e-5

--- ccl_test4cc3d_without_cupy_multi.py
+++ ccl_test4cc3d_without_cupy_multi.py
@@ -8,21 +8,15 @@ import pandas as pd
 from scipy.interpolate import griddata
 import _multiprocessing
 import os
-# TODO: first test how to give multi core different thing ( value or list)
-# TODO: 1. read every group if np.sum !=0 then save that and save the name of group as a list for multi read
-# TODO: 2. multi processing
 GlobalList = [[]]
-print(len(GlobalList[-1]))
 XGlobalMin = 0
 XGlobalMAX = 0
 YGlobalMIN = 0
 YGlobalMAX = 0
 ThreadValue = 0.01
-GridValue = 1e-3
+GridValue = 1e-4
 def clip():
-    print("readcsvbefore", datetime.datetime.now())
     boxTextReadFromPandas = pd.read_csv('boxTest.csv')
-    print("readcsvafter", datetime.datetime.now())
     global XGlobalMAX
     global XGlobalMin
     global YGlobalMIN
@@ -44,12 +38,11 @@ def clip():
         GridZ = GridZ.astype(np.int32)
         if np.sum(GridZ) == 0:
             if(len(GlobalList[-1])!=0):
-                break
                 GlobalList.append([])
             continue
         else:
             GlobalList[-1].append(DividedByPointZGroup.tolist())
-path = "ccl4merge"
+
 def cclonestep(i):
     LabelsInMatrix = []
     VolumnMatrix = []
@@ -57,74 +50,96 @@ def cclonestep(i):
     XMatrix = []
     YMatrix = []
     ZMatrix = []
-    FilePath = os.path.join(path,i+".csv")
-    with open(FilePath, 'w') as NewFile:
-        global GlobalList
-        for DividedByPointZGroup in GlobalList[i]:
-            DividedByPointZGroup = np.asarray(DividedByPointZGroup)
-            Points = DividedByPointZGroup[:, [6, 7]]
-            AlphaLiquid = DividedByPointZGroup[:, 1]
-            V = DividedByPointZGroup[:, 0]
-            X = DividedByPointZGroup[:, 6]
-            Y = DividedByPointZGroup[:, 7]
-            Z = DividedByPointZGroup[:, 5]
-            global XGlobalMAX
-            global XGlobalMin
-            global YGlobalMIN
-            global YGlobalMAX
-            global GridValue
-            global ThreadValue
-            GridX, GridY = np.mgrid[XGlobalMin:XGlobalMAX:GridValue, YGlobalMIN:YGlobalMAX:GridValue]
-            GridZ = griddata(Points, AlphaLiquid, (GridX, GridY), method='nearest')
-            GridAlpha = griddata(Points, AlphaLiquid, (GridX, GridY), method='nearest')
-            GridVolumn = griddata(Points, V, (GridX, GridY), method='nearest')
-            GridCoordinateX = griddata(Points, X, (GridX, GridY), method='nearest')
-            GridCoordinateY = griddata(Points, Y, (GridX, GridY), method='nearest')
-            GridCoordinateZ = griddata(Points, Z, (GridX, GridY), method='nearest')
-            GridZ[GridZ < ThreadValue] = 0
-            GridZ[GridZ >= ThreadValue] = 1
-                        # VolumnMatrix = np.power(np.asarray(VolumnMatrix),1/3)
-            LabelsInMatrix.append(GridZ.tolist())
-            AlphaMatrix.append(GridAlpha.tolist())
-            VolumnMatrix.append(GridVolumn.tolist())
-            XMatrix.append(GridCoordinateX.tolist())
-            YMatrix.append(GridCoordinateY.tolist())
-            ZMatrix.append(GridCoordinateZ.tolist())
-        LabelsOutMatrix = cc3d.connected_components(LabelsInMatrix)
-        N = np.max(LabelsOutMatrix)
-        for segid in range(1, N + 1):
-            # calculate delimeter
-            VolumnMatrix = np.asarray(VolumnMatrix)
-            AlphaMatrix = np.asarray(AlphaMatrix)
-            tmpMatrix = np.asarray(LabelsOutMatrix == segid)
-            sumtmpMatrix = np.multiply(VolumnMatrix, np.multiply(AlphaMatrix, tmpMatrix))
-            sumVolumn = np.sum(sumtmpMatrix)  # sum of Volumn
-            sumVolumnDelimeter = math.pow(sumVolumn * 6 / np.pi, 1 / 3)
+    FilePath = os.path.join(tmppath,str(i)+".csv")
+    global GlobalList
+    for DividedByPointZGroup in GlobalList[i]:
+        DividedByPointZGroup = np.asarray(DividedByPointZGroup)
+        Points = DividedByPointZGroup[:, [6, 7]]
+        AlphaLiquid = DividedByPointZGroup[:, 1]
+        V = DividedByPointZGroup[:, 0]
+        X = DividedByPointZGroup[:, 6]
+        Y = DividedByPointZGroup[:, 7]
+        Z = DividedByPointZGroup[:, 5]
+        global XGlobalMAX
+        global XGlobalMin
+        global YGlobalMIN
+        global YGlobalMAX
+        global GridValue
+        global ThreadValue
+        GridX, GridY = np.mgrid[XGlobalMin:XGlobalMAX:GridValue, YGlobalMIN:YGlobalMAX:GridValue]
+        GridZ = griddata(Points, AlphaLiquid, (GridX, GridY), method='nearest')
+        GridAlpha = griddata(Points, AlphaLiquid, (GridX, GridY), method='nearest')
+        GridVolumn = griddata(Points, V, (GridX, GridY), method='nearest')
+        GridCoordinateX = griddata(Points, X, (GridX, GridY), method='nearest')
+        GridCoordinateY = griddata(Points, Y, (GridX, GridY), method='nearest')
+        GridCoordinateZ = griddata(Points, Z, (GridX, GridY), method='nearest')
+        GridZ[GridZ < ThreadValue] = 0
+        GridZ[GridZ >= ThreadValue] = 1
+        GridZ = GridZ.astype(np.int32)
+        LabelsInMatrix.append(GridZ.tolist())
+        AlphaMatrix.append(GridAlpha.tolist())
+        VolumnMatrix.append(GridVolumn.tolist())
+        XMatrix.append(GridCoordinateX.tolist())
+        YMatrix.append(GridCoordinateY.tolist())
+        ZMatrix.append(GridCoordinateZ.tolist())
+    LabelsInMatrix = np.asarray(LabelsInMatrix)
+    LabelsOutMatrix = cc3d.connected_components(LabelsInMatrix)
+    N = np.max(LabelsOutMatrix)
+    Tmp4SaveList = []
+    for segid in range(1, N + 1):
+        # calculate delimeter
+        VolumnMatrix = np.asarray(VolumnMatrix)
+        AlphaMatrix = np.asarray(AlphaMatrix)
+        tmpMatrix = np.asarray(LabelsOutMatrix == segid)
+        sumtmpMatrix = np.multiply(VolumnMatrix, np.multiply(AlphaMatrix, tmpMatrix))
+        sumVolumn = np.sum(sumtmpMatrix)  # sum of Volumn
+        sumVolumnDelimeter = math.pow(sumVolumn * 6 / np.pi, 1 / 3)
 
-            # coordinate
-            XMatrix = np.asarray(XMatrix)
-            YMatrix = np.asarray(YMatrix)
-            ZMatrix = np.asarray(ZMatrix)
-            MeanX = np.sum(np.multiply(XMatrix, sumtmpMatrix)) / sumVolumn
-            MeanY = np.sum(np.multiply(YMatrix, sumtmpMatrix)) / sumVolumn
-            MeanZ = np.sum(np.multiply(ZMatrix, sumtmpMatrix)) / sumVolumn
-            # write into file
-            NewFile.write(str(sumVolumnDelimeter)
-                          + ',' + str(sumVolumn)
-                          + ',' + str(MeanX)
-                          + ',' + str(MeanY)
-                          + ',' + str(MeanZ)
-                          + '\n')
-    print(i,datetime.datetime.now())
+        # coordinate
+        XMatrix = np.asarray(XMatrix)
+        YMatrix = np.asarray(YMatrix)
+        ZMatrix = np.asarray(ZMatrix)
+        MeanX = np.sum(np.multiply(XMatrix, sumtmpMatrix)) / sumVolumn
+        MeanY = np.sum(np.multiply(YMatrix, sumtmpMatrix)) / sumVolumn
+        MeanZ = np.sum(np.multiply(ZMatrix, sumtmpMatrix)) / sumVolumn
+        # write into file
+        Tmp4SaveList.append([sumVolumnDelimeter,sumVolumn,MeanX,MeanY,MeanZ])
+    np.savetxt(FilePath, Tmp4SaveList)
+            # NewFile.write(str(sumVolumnDelimeter)
+            #               + ',' + str(sumVolumn)
+            #               + ',' + str(MeanX)
+            #               + ',' + str(MeanY)
+            #               + ',' + str(MeanZ)
+            #               + '\n')
+    print(i/len(GlobalList),datetime.datetime.now())
 def ccl():
     pool = multiprocessing.Pool()
-    global GlobalList
     for i in range(len(GlobalList)):
         pool.apply_async(cclonestep,(i,))
     pool.close()
     pool.join()
+
+def merge():
+    tmp = pd.DataFrame(columns=["delimeter","Volumn","MeanX","MeanY","MeanZ"])
+    for i in os.listdir(tmppath):
+        dir_path = os.path.join(tmppath,i)
+        tmp = tmp.append(pd.read_csv(dir_path,header=None,sep='\s+',))
+    np.savetxt('cc3d.csv',tmp)
+
 if __name__ == '__main__':
+    tmppath = r'ccl4merge'
+    if not os.path.exists(tmppath):
+        os.makedirs(tmppath)
+    for i in os.listdir(tmppath):
+        dir_path = os.path.join(tmppath, i)
+        os.remove(dir_path)
+    print("start",datetime.datetime.now())
     clip()
     print("clipend",datetime.datetime.now())
-    print(GlobalList)
     ccl()
+    merge()
+    for i in os.listdir(tmppath):
+        dir_path = os.path.join(tmppath, i)
+        os.remove(dir_path)
+    os.removedirs(tmppath)
+    print("end",datetime.datetime.now())
--- multitaskfirst.py
+++ multitaskfirst.py
@@ -7,7 +7,7 @@ import multiprocessing
 import matplotlib.pyplot as plt
 lower_T=1500000 # 温度被乘了1000
 
-def tt(files):
+def GetMinSingleCore(files):
     dir_path = os.path.join(path, files[1])
     print(dir_path, files[0],time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     test = pd.read_csv(dir_path,header=None,skiprows=[0,1],sep='\s+',)
@@ -18,7 +18,7 @@ def tt(files):
     nn.reset_index(drop=True, inplace=True)
     nn= pd.DataFrame(data=nn,dtype=np.int)
     tmp=nn[(nn['T']>=lower_T)].groupby('y')['x'].idxmin()
-    tt = []
+    TmpList4MIN = []
     for i in tmp:
         if nn.loc[i, :]['y'] == nn.loc[i - 1, :]['y']:
             # print(nn.loc[i,:],nn.loc[i-1,:])
@@ -26,27 +26,27 @@ def tt(files):
             s = nn.loc[i - 1, :]
             # interpolating
             xd = (lower_T - s['T']) / (b['T'] - s['T']) * (b['x'] - s['x']) + s['x']
-            tt.append([xd,b['y']])
-    tt = pd.DataFrame(tt,columns=['x','y'])
-    x = tt['y']
-    y = tt['x']
+            TmpList4MIN.append([xd,b['y']])
+    TmpList4MIN = pd.DataFrame(TmpList4MIN,columns=['x','y'])
+    x = TmpList4MIN['y']
+    y = TmpList4MIN['x']
     spl = interpolate.splrep(x, y)
     x2 = np.arange(x.min(),x.max(),0.1)
     y2 = interpolate.splev(x2, spl)
     xnew = y[:y.idxmin()]  # x
     ynew = x[:y.idxmin()]  # y
-    ttt = [[y2.min(),x2[y2.argmin()],np.degrees(np.arctan((xnew.max()-xnew.min())/(ynew.max()-ynew.min())))]]
-    tttdir = os.path.join(tmppath,files[1])
-    np.savetxt(tttdir,ttt)
-def getmin(path):
+    MinValue = [[y2.min(),x2[y2.argmin()],np.degrees(np.arctan((xnew.max()-xnew.min())/(ynew.max()-ynew.min())))]]
+    MinValuedir = os.path.join(tmppath,files[1])
+    np.savetxt(MinValuedir,MinValue)
+def GetMinMulti(path):
     filelist = os.listdir(path)
-    cnt = 0
     pool = multiprocessing.Pool()
     for files in enumerate(filelist):
-        cnt+=1
-        pool.apply_async(tt,(files,))
+        pool.apply_async(GetMinSingleCore,(files,))
     pool.close()
     pool.join()
+
+# megrge all the file under the tmp dir
 def merge():
     tmp = pd.DataFrame()
     for i in os.listdir(tmppath):
@@ -56,14 +56,16 @@ def merge():
 
 if __name__=='__main__':
     paths = [r"a20/dataToThomas/a20"]
+    # mkdir a file "a20tmp", if exits delete the files under the path
     tmppath = r'a20tmp'
     if not os.path.exists(tmppath):
         os.makedirs(tmppath)
     for i in os.listdir(tmppath):
         dir_path = os.path.join(tmppath,i)
         os.remove(dir_path)
+
     for path in paths:
-        getmin(path)
+        GetMinMulti(path)
         merge()
         for i in os.listdir(tmppath):
             dir_path = os.path.join(tmppath,i)
